9a2bf10657775fea048039b27d3d0505762343a2
Christian Baños <christiandepaulbp@gmail.com>
2024-11-16 13:54:04 +0000

    1. añadi el logo con formato npg a public. 2. empece con la creacion de la landing page 3. cree un componente para el heder de mi landing

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header_landingPage/header";
+
+function page() {
+  return (
+    <div className="">
+      <Header />
+    </div>
+  );
+}
+
+export default page;
---
+++ src/components/Header_landingPage/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div>
+      <header className="flex felx-wrap items-center justify-start max-w-5xl p-4 mx-auto md:py-8">
+        <img src="/logo.png" alt="Logo de Crewland" />
+      </header>
+    </div>
+  );
+}
