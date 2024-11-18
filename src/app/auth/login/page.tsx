b5e419057af4e888642b699e468d35d03fafab58
Debi312 <deboragarciaescudero@gmail.com>
2024-11-18 14:03:11 +0100

    style: add background image, input icons, custom checkbox; fix: correct forgot your password redirection path; implement simple checkbox function

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,98 +1,129 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ReusableInput from "@/components/elements/Inputs/ReusableInput";
 import Button from "@/components/elements/Buttons/Button";
 import Title from "@/components/elements/Titles/Title";
+import {User} from "lucide-react";
+
 export default function Login() {
-const router = useRouter()
-  const  handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const router = useRouter()
+
+  const [rememberMe, setRememberMe] = useState(false)
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRememberMe(event.target.checked)
+  }
+  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
     const formData = new FormData(event.currentTarget)
     const email = formData.get("email") as string
     const password = formData.get("contraseña") as string
 
-    if(email&&password){
-      router.push("/home")
+    if (email && password) {
+      if (rememberMe) {
+        //localStorage.setItem("rememberMe", "true");
+      }
+      router.push("/home");
     } else {
-      console.error("Fields are required")
+      console.error("Fields are required");
     }
-
   }
 
   return (
     <>
-      <header className=" absolute top-8 left-0 m-4 bg-background">
-      <div className="bg-white rounded-md p-1">
-        <div className="bg-black invert w-[50px] h-[50px] rounded-md">
-          <Image
-            src="/crewland_logo.svg"
-            alt="Crewland logo"
-            fill
-          />
-        </div>
-      </div>
-    </header>
-    <div className=" flex flex-col justify-center bg-background min-h-screen">
-      <section className="mt-32">
-      <div className="flex flex-row justify-center items-center mb-8">
-        <Title
-          text="CREWLAND"       
-          className=" text-[35px]"
-        />
-        <Image
-          className="white top-[0px]"
-          src="/crewland_logo.svg"
-          alt="Crewland logo"
-          width={60}
-          height={50}
-        />
-      </div>
-  
-      <form onSubmit={handleLoginSubmit}  className="flex flex-col mx-4 py-4 gap-6">
-        <div className="flex flex-col">
-              <ReusableInput
-            id="email"
-            label=""
-            type="email"
-            placeholder="Correo Electronico"
-          />
-            
-          <span></span>
-        </div>
-        <div className="flex flex-col">
-              <ReusableInput
-            id="contraseña"
-            label=""
-            type="password"
-            placeholder="Contraseña"
-          />
+      <header className=" absolute top-8 left-0 m-4 bg-background rounded-md z-10">
+
+        <div className="bg-white rounded-md p-1 ">
+          <div className="bg-black invert w-[50px] h-[50px] rounded-md">
+            <Image
+              src="/crewland_logo.svg"
+              alt="Crewland logo"
+              fill
+            />
           </div>
-          <div className="flex flex-row items-center gap-2">
-            <input
-              type="checkbox"
-              id="rememberMe"
-              className="w-5 h-5 appearance-none border-2 border-customWhite rounded-sm bg-transparent checked:bg-transparent checked:border-customWhite checked:text-primary cursor-pointer"
-              />
-          <label htmlFor="rememberMe" className="text-customWhite">
-    Recordar contraseña
-  </label>
+        </div>
+      </header>
+      <div
+        className="relative flex flex-col justify-center bg-background min-h-screen"
+        style={{
+          backgroundImage: "url(/login.png)",
+          backgroundPosition: "center 100px",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat"
+        }}
+      >
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-background"
+          style={{
+            opacity: 0.5,
+          }}
+        ></div>
+        <section className="relative z-10 mt-32">
+          <div className="flex flex-row justify-center items-center mb-8">
+            <Title
+              text="CREWLAND"
+              className=" text-[35px]"
+            />
+            <Image
+              className="white top-[0px]"
+              src="/crewland_logo.svg"
+              alt="Crewland logo"
+              width={60}
+              height={50}
+            />
           </div>
 
-        <div className="flex flex-col gap-4">
-        <Button text="Iniciar Sesión" variant="primary" />
-        <Button text="Registrarse" variant="ghost" onClick={() => router.push("/auth/register")} />
-        </div>
-      </form>
-      <div className="flex flex-col items-center">
-      <Link href="" className="text-customWhite  hover:text-primary underline underline-offset-2 decoration-1">
-          ¿Olvidaste tu contraseña?
-        </Link>
-        </div>
+          <form onSubmit={handleLoginSubmit} className="flex flex-col mx-4 py-4 gap-6">
+            <div className="flex flex-col">
+              <ReusableInput
+                id="email"
+                label=""
+                type="email"
+                placeholder="Correo Electronico"
+              />
+    
+              <span>
+              <User className=' text-customWhite absolute top-[110px] right-4'/>
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <ReusableInput
+                id="contraseña"
+                label=""
+                type="password"
+                placeholder="Contraseña"
+                password={true}
+              />
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <input
+                type="checkbox"
+                id="rememberMe"
+                name="rememberMe"
+                checked={rememberMe}
+                onChange={handleCheckboxChange}
+                className={`w-5 h-5 appearance-none border-2 rounded-sm bg-transparent cursor-pointer 
+    ${rememberMe ? "border-primary checked:bg-primary" : "border-customWhite"} `}
+              />
+              <label htmlFor="rememberMe" className="text-customWhite">
+                Recordar contraseña
+              </label>
+            </div>
+            <div className="flex flex-col gap-4">
+              <Button text="Iniciar Sesión" variant="primary" />
+              <Button text="Registrarse" variant="ghost" onClick={() => router.push("/auth/register")} />
+            </div>
+          </form>
+          <div className="flex flex-col items-center">
+            <Link href="/auth/recovery" className="text-customWhite  hover:text-primary underline underline-offset-2 decoration-1">
+              ¿Olvidaste tu contraseña?
+            </Link>
+          </div>
         </section>
       </div>
-      </>
+    </>
   )
 }
