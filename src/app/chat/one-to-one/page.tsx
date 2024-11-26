5696a3c3e25e0132ca0974d2bb595b6833a7b142
Debi312 <deboragarciaescudero@gmail.com>
2024-11-26 09:40:58 +0100

    fix: refactor code to use mock data for messages and use and prepare component for future socket integration

--- src/app/chat/one-to-one/page.tsx
+++ src/app/chat/one-to-one/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+
+import { user, messages } from '@/json/onteToOneChat'
+import { useState } from 'react'
 import Container from '@/components/elements/Container/Container'
 import Image from 'next/image'
-import Title from '@/components/elements/Titles/Title'
 import { Heart, EllipsisVertical, CheckCheck, Smile, SendHorizontal } from 'lucide-react'
 
 export default function OneToOne() {
+    const [isLiked, setIsLiked] = useState(user.like);
+
+    const handleToggleLike = () => {
+        setIsLiked(!isLiked);
+    }
     return (
         <Container>
             <section className='flex flex-col w-full h-screen'>
@@ -15,7 +23,6 @@ export default function OneToOne() {
                             width={24}
                             height={12}
                         />
-
                         <div className='flex flex-row items-center gap-2'>
                             <Image
                                 src='/user01.png'
@@ -24,54 +31,55 @@ export default function OneToOne() {
                                 height={40}
                             />
                             <div className='flex flex-col justify-center '>
-                                <h3 className='text-xl leading-none'>User Name</h3>
+                                <h3 className='text-xl leading-none'>{user.name}</h3>
                                 <div className='flex flex-row gap-2 items-center'>
-                                    <span className='bg-[#26874A] rounded-full w-2 h-2 inline-block'></span>
-                                    <p className='text-[10px] text-gray-400 font-normal'>Conectado</p>
+                                    <span className={`bg-${user.status === 'online' ? '[#26874A]' : '[#FA8080]'} rounded-full w-2 h-2 inline-block`}></span>
+                                    <p className='text-[10px] text-gray-400 font-normal'>{user.status === 'online' ? 'Conectado' : 'Desconectado'}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className='flex flex-row gap-2'>
-                        <Heart className='text-white fill-white w-[20px]' />
+                        <button onClick={handleToggleLike}>
+                            <Heart className={`text-white w-[20px] ${isLiked ? 'fill-white' : ''}`} />
+                        </button>
                         <EllipsisVertical className='text-white' />
                     </div>
                 </header>
-                <main className='flex flex-col w-full h-full justify-start items-center mt-28 gap-2'>
 
-                    <div>
-                        <div className='text-[black] text-[14px] rounded-3xl rounded-br-none bg-primary p-4'>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <main className='flex flex-col w-full h-full justify-start items-center mt-28 gap-2 px-4'>
+                    {messages.map((message) => (
+                        <div
+                            key={message.id}
+                            className={`flex flex-col ${
+                                message.sentByCurrentUser ? 'items-end' : 'items-start'
+                            } w-full`}>
+                            <div className={`text-[14px] rounded-3xl ${
+                                    message.sentByCurrentUser ? 'rounded-br-none bg-primary text-black' : 'rounded-bl-none bg-customWhite text-black'
+                                } p-4`}>
+                                <p>{message.text}</p>
+                            </div>
+
+                            <div className={`flex ${message.sentByCurrentUser ? 'justify-end' : 'justify-start'} items-center gap-2 mt-[2px] min-h-[20px]`}>
+                                <span className='text-[10px] text-gray-400'>{message.timestamp}</span>
+                                {message.sentByCurrentUser && (
+                                    <CheckCheck className={`text-[${message.isRead ? '#62C0FA' : 'white'}] w-[20px]`} />
+                                )}
+                            </div>
                         </div>
-                        <div className='flex justify-end items-center gap-2 mt-[2px] min-h-[20px]'>
-                            <span className="text-[10px] text-gray-400 ">12:47 pm</span>
-                            <CheckCheck className='text-white w-[20px]' />
-                        </div>
-                    </div>
-                    <div>
-                        <div className='text-black text-[14px] rounded-3xl rounded-bl-none bg-customWhite p-4'>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        </div>
-                        <div className='flex justify-start items-center gap-2 mt-[2px] min-h-[24px]'>
-                            <span className="text-[10px] text-gray-400 items-end">12:47 pm</span>
-                        </div>
-                    </div>
+                    ))}
                 </main>
 
-                <footer className='fixed bottom-0 flex flex-row items-center left-0 w-full px-4 py-3 '>
-                    <div className="relative w-full flex items-center rounded-lg border-[1px]  bg-transparent">
-                        <Smile
-                            className="absolute left-3 "
-                        />
+                <footer className='fixed bottom-0 flex flex-row items-center left-0 w-full px-4 py-3'>
+                    <div className='relative w-full flex items-center rounded-lg border-[1px] bg-transparent'>
+                        <Smile className='absolute left-3' />
                         <input
                             className='w-full p-3 pl-10 bg-transparent text-customWhite outline-none'
-                            type="text"
-                            placeholder="Escribir mensaje..."
+                            type='text'
+                            placeholder='Escribir mensaje...'
                         />
                         <div className='absolute right-3 flex justify-center items-center w-8 h-8 bg-primary rounded-full'>
-                            <SendHorizontal
-                                className="text-black"
-                            />
+                            <SendHorizontal className='text-black' />
                         </div>
                     </div>
                 </footer>
