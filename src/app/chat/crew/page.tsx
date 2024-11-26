15a008ea5371cde69415bba6a758d23c0161fccb
Debi312 <deboragarciaescudero@gmail.com>
2024-11-26 11:23:34 +0100

    feat: add button into Explorar grupos card

--- src/app/chat/crew/page.tsx
+++ src/app/chat/crew/page.tsx
@@ -104,7 +104,7 @@ export default function ChatCrew() {
                                 <ul className="grid grid-cols-2 gap-4">
                                     {chat.chatGroups.map((group, index) => (
                                         <li key={index} className="rounded-lg p-2 bg-primary">
-                                            <div className="flex flex-col gap-5">
+                                            <div className={`flex flex-col ${chat.title === "Explorar grupos" ? "gap-2" : "gap-5"}`}>
                                                 <p className=" flex flex-row justify-start text-black">{group.name}</p>
                                                 <div className="flex flex-row justify-between items-center">
                                                     <div className="flex flex-row justify-start -space-x-2">
@@ -118,6 +118,12 @@ export default function ChatCrew() {
                                                     </div>
                                                     <p className="text-black text-xs">{group.time}</p>
                                                 </div>
+                                                {chat.title === "Explorar grupos" && (
+                                                    <button className=" text-background bg-primary border-[1.5px] border-black rounded-lg focus:border-customWhite focus:text-customWhite">
+                                                        Unirse
+                                                    </button>
+                                                )}
+
                                             </div>
                                         </li>
                                     ))}
@@ -127,7 +133,7 @@ export default function ChatCrew() {
                     ))}
                 </main>
             </section>
-            <footer className="fixed flex flex-row justify-around bottom-0 gap-3 w-full h-16">
+            <footer className="fixed flex flex-row justify-around bottom-0 gap-3 w-full h-16 bg-background">
                 <p>Search</p>
                 <p>Home</p>
                 <p>Crew</p>
