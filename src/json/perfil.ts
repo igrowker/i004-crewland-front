9b21bda9246c990173a1f57229f7ed649e93dda4
juanRCoder <guillecarck@gmail.com>
2024-12-02 16:50:29 -0500

    feat: agregar interfaz de configuracion

---
+++ src/json/perfil.ts
@@ -0,0 +1,14 @@
+export const configPerfil = [
+  {
+    src: 'quiz',
+    text: 'FAQ'
+  },
+  {
+    src: 'article_shortcut',
+    text: 'Términos y condiciones'
+  },
+  {
+    src: 'gpp_maybe',
+    text: 'Política de privacidad.'
+  }
+]
