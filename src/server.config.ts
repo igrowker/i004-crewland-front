a933ceec87c070e0ffe52a8f30effedb6bbe1cff
juanRCoder <guillecarck@gmail.com>
2024-12-03 12:30:14 -0500

    refactor:  agregar solicitud para registrar

--- src/server.config.ts
+++ src/server.config.ts
@@ -1,3 +1,5 @@
-const server = 'http://localhost:3000';
+const server = 'http://crewland.mooo.com:3000' // test con devops
+//const server = 'https://i004-crewland-back.onrender.com'; // test en render de ulises
+//const server = 'http://localhost:3000'; // test local
 
 export default server;
