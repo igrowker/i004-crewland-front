8442d93aa048f6639ba9f2bc45d9b1f7be7acc20
Alicia Gálvez <136130096+alicia4079@users.noreply.github.com>
2024-12-07 11:43:51 -0300

    Alicia brunch - fix profile (#68)
    * feat profile connect back front
    
    * add profile name

---
+++ src/services/api/profile.ts
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import { token } from '@/server.config'
+import { UserInterface } from '@/interfaces/user'
+
+export const getUserById = async (id: string) => {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_SERVER}/users/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    return response.data.data
+  } catch (e) {
+    console.error(`Error al obtener el usuario con ID ${id}:`, e)
+  }
+}
+export const patchUser = async (
+  id: string,
+  updateData: Partial<UserInterface>
+) => {
+  try {
+    const response = await axios.patch(
+      `${process.env.NEXT_PUBLIC_SERVER}/users/${id}`,
+      updateData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    return response.data
+  } catch (e) {
+    console.error(`Error al actualizar el usuario con ID ${id}:`, e)
+  }
+}
